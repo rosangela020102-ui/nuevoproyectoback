a21b654af761a2895e19c0e57e222b277a09031b
Rosangela Rodriguez <rosangela020102@gmail.com>
2026-07-06 16:57:29 +0200

    docs: swagger configurado con archivo json

--- prisma.config.ts
+++ prisma.config.ts
@@ -7,6 +7,6 @@ export default defineConfig({
     path: "prisma/migrations",
   },
   datasource: {
-    url: "postgresql://postgres.vcrvbszinjnqrukoxrxn:Rossy2026Back@aws-0-eu-north-1.pooler.supabase.com:5432/postgres?sslmode=require&supavisor_session=true",
+    url: "postgresql://postgres.vcrvbszinjnqrukoxrxn:Rossy2026Back@db.vcrvbszinjnqrukoxrxn.supabase.co:5432/postgres?sslmode=require&connect_timeout=30",
   },
 });
--- src/app.js
+++ src/app.js
@@ -1,7 +1,9 @@
 import express from "express";
 import cors from "cors";
 import swaggerUi from "swagger-ui-express";
-import swaggerJsDoc from "swagger-jsdoc";
+import fs from "fs";
+import path from "path";
+
 import { notFound } from "./middlewares/notFound.js";
 import { errorHandler } from "./middlewares/errorHandler.js";
 
@@ -10,39 +12,22 @@ import authRoutes from "./routes/auth.routes.js";
 import cartRoutes from "./routes/cart.routes.js";
 import reviewRoutes from "./routes/review.routes.js";
 import wishlistRoutes from "./routes/wishlist.routes.js";
+import authController from "./controllers/auth.controller.js";
+import { protect } from "./middlewares/auth.middleware.js";
 
 const app = express();
 
 app.use(express.json());
+app.use(cors({ origin: true, credentials: true }));
 
-app.use(cors({
-  origin: true,
-  credentials: true
-}));
 
-const swaggerOptions = {
-  definition: {
-    openapi: "3.0.0",
-    info: {
-      title: "API Ecommerce - React Ready",
-      version: "1.0.0",
-      description: "Documentación oficial de los endpoints (PostgreSQL + MongoDB)",
-    },
-    servers: [
-      {
-        url: "http://localhost:3000",
-        description: "Servidor Local"
-      }
-    ],
-  },
-  apis: ["./src/routes/*.js"], 
-};
-
-const swaggerDocs = swaggerJsDoc(swaggerOptions);
-app.use("/api/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+const swaggerDocument = JSON.parse(
+  fs.readFileSync(path.resolve("./swagger.json"), "utf8")
+);
+app.use("/api/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.get("/", (req, res) => {
-  res.json({ success: true, message: "API Funcionando - Backend React Ready" });
+  res.json({ success: true, message: "API Funcionando" });
 });
 
 app.use("/api/auth", authRoutes);
@@ -51,7 +36,8 @@ app.use("/api/products", reviewRoutes);
 app.use("/api/cart", cartRoutes);
 app.use("/api/wishlist", wishlistRoutes);
 
-// Manejo de errores
+app.get("/api/me", protect, authController.getMe); 
+
 app.use(notFound);
 app.use(errorHandler);
 
